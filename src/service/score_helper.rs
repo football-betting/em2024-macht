c6a5cb863b8fd8f7b1f2ddfc56f3f04118c56238
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2024-06-30 21:00:32 +0200

    get right score, we tipp only 90 min

--- src/service/score_helper.rs
+++ src/service/score_helper.rs
@@ -5,8 +5,13 @@ pub struct ScoreHelper {}
 impl ScoreHelper {
     pub fn set_home_and_away_score(matches: &mut Vec<Match>) {
         for single_match in matches {
-            single_match.homeScore = single_match.score.fullTime.home;
-            single_match.awayScore = single_match.score.fullTime.away;
+            if let Some(regular_time) = &single_match.score.regularTime {
+                single_match.homeScore = regular_time.home;
+                single_match.awayScore = regular_time.away;
+            } else {
+                single_match.homeScore = single_match.score.fullTime.home;
+                single_match.awayScore = single_match.score.fullTime.away;
+            }
         }
     }
 }
